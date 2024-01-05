8a5cd895c2cea29f8917c530b950f8a66e93010c
elecun <elecun2@gmail.com>
2024-01-05 11:05:27 +0900

    fixed

--- vision/camera/gige.py
+++ vision/camera/gige.py
@@ -181,7 +181,7 @@ def gige_camera_discovery() -> list:
         for idx, cam in enumerate(_camera_array_container):
             cam.Attach(_tlf.CreateDevice(_devices[idx]))
             _model_name = cam.GetDeviceInfo().GetModelName()
-            _ip_addr = cam.GetIpAddress()
+            _ip_addr = _devices[idx].GetIpAddress()
             print(_ip_addr)
             #print(f"Using device {_model_name}({_ip_addr})")
             _caminfo_array.append(tuple(idx, _model_name, _ip_addr))
